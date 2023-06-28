1966b4f7ee5cb39574b0bd25c432f4ffbb7ecad4
Zachary Mann <zacharym98@outlook.com>
2023-06-27 20:19:07 -0600

    Preparing for deployment

--- zetaPsi/settings.py
+++ zetaPsi/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&B!716Ls7pEg1h&Y@%78h$jT4XJ!wi
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['muthetazetapsiwebsite-73ea06594d39.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['muthetazetapsiwebsite-73ea06594d39.herokuapp.com','mutheta-0f1d1d9db025.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
