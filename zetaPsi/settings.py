88ba21cf122574c97f7abd5e1386bcc826a5c690
Zachary Mann <zacharym98@outlook.com>
2023-07-01 16:08:51 -0600

    Configured S3 Bucketeer Storage

--- zetaPsi/settings.py
+++ zetaPsi/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'zetaPsi.wsgi.application'
 # Uncomment out for local database
 # Remember to comment out dj_database_url and related lines
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
