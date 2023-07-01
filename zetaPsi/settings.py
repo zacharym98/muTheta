4ea96d01055560c68d030a7a709efa8ad22a7358
Zachary Mann <zacharym98@outlook.com>
2023-07-01 16:13:58 -0600

    Configured S3 Bucketeer Storage

--- zetaPsi/settings.py
+++ zetaPsi/settings.py
@@ -141,8 +141,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # The following configs determine if files get served from the server or an S3 storage
-S3_ENABLED = config('S3_ENABLED', cast=bool, default=False)
-LOCAL_SERVE_MEDIA_FILES = config('LOCAL_SERVE_MEDIA_FILES', cast=bool, default=not S3_ENABLED)
+S3_ENABLED = config('S3_ENABLED', cast=bool, default=True)
+LOCAL_SERVE_MEDIA_FILES = config('LOCAL_SERVE_MEDIA_FILES', cast=bool, default=S3_ENABLED)
 LOCAL_SERVE_STATIC_FILES = config('LOCAL_SERVE_STATIC_FILES', cast=bool, default=not S3_ENABLED)
 
 if (not LOCAL_SERVE_MEDIA_FILES or not LOCAL_SERVE_STATIC_FILES) and not S3_ENABLED:
