7a2d196ec0422f72944510a1fb0b82b3b71b2fe0
Zachary Mann <zacharym98@outlook.com>
2023-07-01 17:33:39 -0600

    Reconfigured for postgres, don't use bucketeer

--- zetaPsi/settings.py
+++ zetaPsi/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import environ
 import django_heroku
-from decouple import config
 
 env = environ.Env()
 environ.Env.read_env()
@@ -141,38 +140,6 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# The following configs determine if files get served from the server or an S3 storage
-S3_ENABLED = config('S3_ENABLED', cast=bool, default=True)
-LOCAL_SERVE_MEDIA_FILES = config('LOCAL_SERVE_MEDIA_FILES', cast=bool, default=S3_ENABLED)
-LOCAL_SERVE_STATIC_FILES = config('LOCAL_SERVE_STATIC_FILES', cast=bool, default=not S3_ENABLED)
-
-if (not LOCAL_SERVE_MEDIA_FILES or not LOCAL_SERVE_STATIC_FILES) and not S3_ENABLED:
-    raise ValueError('S3_ENABLED must be true if either media or static files are not served locally')
-
-if S3_ENABLED:
-    AWS_ACCESS_KEY_ID = config('BUCKETEER_AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = config('BUCKETEER_AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = config('BUCKETEER_BUCKET_NAME')
-    AWS_S3_REGION_NAME = config('BUCKETEER_AWS_REGION')
-    AWS_DEFAULT_ACL = None
-    AWS_S3_SIGNATURE_VERSION = config('S3_SIGNATURE_VERSION', default='s3v4')
-    AWS_S3_ENDPOINT_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    print("S3 Enabled!")
-    print(AWS_S3_ENDPOINT_URL)
-
-if not LOCAL_SERVE_STATIC_FILES:
-    STATIC_DEFAULT_ACL = 'public-read'
-    STATIC_LOCATION = 'static'
-    STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'zetaPsi.storage_backends.StaticStorage'
-
-if not LOCAL_SERVE_MEDIA_FILES:
-    PUBLIC_MEDIA_DEFAULT_ACL = 'public-read'
-    PUBLIC_MEDIA_LOCATION = 'media/public'
-
-    MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'zetaPsi.storage_backends.PublicMediaStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- zetaPsi/urls.py
+++ zetaPsi/urls.py
@@ -23,9 +23,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls'))
 ]
-
-if settings.LOCAL_SERVE_STATIC_FILES:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-if settings.LOCAL_SERVE_MEDIA_FILES:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
