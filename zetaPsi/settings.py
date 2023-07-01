64c2af897e2701c3a7a95c2ea6e54e7a4f4d1332
Zachary Mann <zacharym98@outlook.com>
2023-07-01 17:00:42 -0600

    Configured S3 Bucketeer Storage

--- zetaPsi/settings.py
+++ zetaPsi/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'zetaPsi.wsgi.application'
 # Uncomment out for local database
 # Remember to comment out dj_database_url and related lines
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -187,8 +187,8 @@ django_heroku.settings(locals())
 
 # Update database configuration from $DATABASE_URL.
 # Comment out when working on database
-# import dj_database_url
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
